2ffac7eaf191b7ee86cb0f64c89c48e840a29d78
NikolayS93 <nikolays93@ya.ru>
2023-06-03 13:37:08 +0400

    Add task stub

---
+++ source/app.ts
@@ -0,0 +1,6 @@
+const totalPrice = ({ price, discount, isInstallment, months }) => {
+    // Your code here...
+};
+
+totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+// 6250
