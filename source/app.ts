1da1d38f43183c6bf7707354145c660260f18b38
NikolayS93 <nikolays93@ya.ru>
2023-06-03 14:14:36 +0400

    Switch right way

--- source/app.ts
+++ source/app.ts
@@ -1,6 +1,16 @@
-const totalPrice = ({ price, discount, isInstallment, months }) => {
-    // Your code here...
-};
+totalPrice({price: 100000, discount: 25, months: 12}); // 6250
 
-totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
-// 6250
+/**
+ * Call total price calculate method.
+ *
+ * @param price price number in currency.
+ * @param discount discount number in percent.
+ * @param months months of instalment.
+ */
+function totalPrice({price, discount, months}: Price): number {
+    return months > 0 ?
+        // Calculate with instalment.
+        totalInstallmentPrice(price, discount, months) :
+        // Calculate common.
+        discountPrice(price, discount);
+}
